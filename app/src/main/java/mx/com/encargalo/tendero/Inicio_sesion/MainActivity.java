5d384063bdd843fa599fdf56f30edb0b95de7e7b
Douglas-BM <douglas71328524@continental.edu.pe>
2022-10-19 17:02:39 -0500

    Segunda prueba

--- app/src/main/java/mx/com/encargalo/tendero/Inicio_sesion/MainActivity.java
+++ app/src/main/java/mx/com/encargalo/tendero/Inicio_sesion/MainActivity.java
@@ -45,7 +45,8 @@ public class MainActivity extends AppCompatActivity {
                 R.id.nav_mis_ordenes,
                 R.id.nav_mi_publicidad,
                 R.id.nav_mi_aprendizaje,
-                R.id.nav_soporte)
+                R.id.nav_soporte,
+                R.id.nav_notificaciones)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
